6d5b4740147c6eff0e1cf168058901717097861b
Roald Bankras <roald.bankras@bynder.com>
2023-07-31 09:20:27 +0200

    2 adding validation and test coverage on config page

--- src/Config.tsx
+++ src/Config.tsx
@@ -1,25 +1,59 @@
-import {useChromeStorageSync} from 'use-chrome-storage';
 import './Config.css';
+import {useEffect, useState} from "react";
 
 function Config() {
-    const [{jiraHost, jiraUser}, setValue, , error] = useChromeStorageSync('config', {jiraHost:'', jiraUser:''});
+    const [config, setConfig] = useState({jiraHost:'', jiraUser:''});
+    const [error, setError] = useState('');
+    useEffect(() => {
+        chrome.storage.sync.get('config').then(({ config }) => setConfig(config))
+    }, [])
 
     function updateHost(e: React.ChangeEvent<HTMLInputElement>) {
-        setValue(config => ({...config, jiraHost:e.target.value}));
+        setConfig(config => ({...config, jiraHost: e.target.value}));
+        setError(() => 'change not saved')
     }
     function updateUser(e: React.ChangeEvent<HTMLInputElement>) {
-        setValue(config => ({...config, jiraUser:e.target.value}));
+        setConfig(config => ({...config, jiraUser: e.target.value}));
+        setError(() => 'change not saved')
+    }
+
+    function validURL(value: string) {
+        let url;
+        try {
+            url = new URL(value);
+        } catch (_) {
+            return false;
+        }
+        return url.protocol === 'http:' || url.protocol === 'https:';
+    }
+
+    function updateConfig(e: React.FormEvent) {
+        e.preventDefault();
+        if(!config.jiraHost || !config.jiraUser) {
+            setError(() => 'both values are required');
+            return;
+        }
+        if(!validURL(config.jiraHost)) {
+            setError(() => 'jiraHost is not a valid URL');
+            return;
+        }
+        if(!config.jiraHost.endsWith('/')) {
+            setError(() => 'jiraHost needs to end with a \'/\'');
+            return;
+        }
+        chrome.storage.sync.set({'config':config}).then(() => setError(() => ''));
     }
 
     return (
-        <form className="Config">
+        <form className="Config" onSubmit={updateConfig}>
             <label>Jira hostname:
-                <input placeholder="https://domain.atlassian.net/" value={jiraHost} onChange={updateHost}/>
+                <input name='jiraHost' placeholder="https://domain.atlassian.net/" value={config.jiraHost} onChange={updateHost}/>
             </label>
             <label>Jira username:
-                <input value={jiraUser} onChange={updateUser}/>
+                <input name='jiraUser' value={config.jiraUser} onChange={updateUser}/>
             </label>
-            { error && <div className={error}>Error: {error}</div> }
+            <input type="submit" value="Save"/>
+            <div>{error}</div>
         </form>
     );
 }
